575e9f3328e4034dcefd9fbb53be4b6ffbdcf29c
Kevin Mattix <kmattrider@gmail.com>
2022-11-23 11:16:38 -0500

    added rest of the games

--- src/components/GameIcon.tsx
+++ src/components/GameIcon.tsx
@@ -4,6 +4,9 @@ import React from 'react'
 import rocketleagueIcon from '../assets/rocketleague.png';
 import valorantIcon from '../assets/valorant.png';
 import leagueoflegendsIcon from '../assets/leagueoflegends.png'
+import smashultimateIcon from '../assets/smashultimate.png';
+import fifaIcon from '../assets/fifa.png';
+import nba2kIcon from '../assets/nba2k.png';
 
 type GameIconProps = {
     game: string
@@ -21,15 +24,15 @@ export default function GameIcon({ game }: GameIconProps) {
         case 'leagueoflegends':
             image = leagueoflegendsIcon;
             break;
-        // case 'smashultimate':
-        //     image = '';
-        //     break;
-        // case 'fifa':
-        //     image = '';
-        //     break;
-        // case 'nba2k':
-        //     image = '';
-        //     break;
+        case 'smashultimate':
+            image = smashultimateIcon;
+            break;
+        case 'fifa':
+            image = fifaIcon;
+            break;
+        case 'nba2k':
+            image = nba2kIcon;
+            break;
         default:
             image = '';
     }
