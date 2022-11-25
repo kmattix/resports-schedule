f505108ab2f1f4d16cf658eb203a1ab389f1d212
Kevin Mattix <kmattrider@gmail.com>
2022-11-25 13:26:33 -0500

    adding rerouting if not signed in

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -3,30 +3,37 @@ import React  from 'react';
 import { useNavigate } from 'react-router-dom';
 import resportsLogo from '../assets/logo192.png';
 import AddMatchForm from './AddMatchForm';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../utils/firebaseService';
 
 export default function Admin() {
     const navigate = useNavigate();
+    const [user] = useAuthState(auth);
     
     return(
+
         <>
-            <IconButton onClick={() => navigate('/')}>
+            {user ? <>
+                <IconButton onClick={() => navigate('/')}>
+                    <Box
+                    component={'img'}
+                    sx={{
+                        maxHeight: 50,
+                        maxWidth: 50,
+                        backgroundColor: 'white',
+                        borderRadius: '50%',
+                        padding: '2px',
+                    }}
+                    alt={'Schedule'}
+                    src={resportsLogo}/>
+                </IconButton>
                 <Box
-                component={'img'}
-                sx={{
-                    maxHeight: 50,
-                    maxWidth: 50,
-                    backgroundColor: 'white',
-                    borderRadius: '50%',
-                    padding: '2px',
-                }}
-                alt={'Schedule'}
-                src={resportsLogo}/>
-            </IconButton>
-            <Box
-            display='flex'
-            justifyContent='center'
-            alignItems='top'>
-                <AddMatchForm/>
-            </Box>
+                display='flex'
+                justifyContent='center'
+                alignItems='top'>
+                    <AddMatchForm/>
+                </Box>
+            </> :
+            navigate('/sign-in')}
         </>);
 }
