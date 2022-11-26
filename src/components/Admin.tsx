7f7c41ff3d9b3453a2e0729584c8be7d47298d86
Kevin Mattix <kmattrider@gmail.com>
2022-11-26 11:29:49 -0500

    added sign out to admin panel

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -1,20 +1,38 @@
-import { Box, IconButton, Tooltip } from '@mui/material';
-import React  from 'react';
+import { Box, IconButton, ListItemIcon, ListItemText, Menu, MenuItem } from '@mui/material';
+import { CalendarMonth, Logout } from '@mui/icons-material';
+import React, { useState }  from 'react';
 import { useNavigate } from 'react-router-dom';
 import resportsLogo from '../assets/logo192.png';
 import AddMatchForm from './AddMatchForm';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../utils/firebaseService';
+import { signOut } from 'firebase/auth';
+
 
 export default function Admin() {
     const navigate = useNavigate();
     const [user] = useAuthState(auth);
+
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    const handleLogout = () => {
+        signOut(auth).then(() => {
+            navigate('/');
+        }).catch((e) => {
+            handleClose();
+        })
+    }
     
     return(<>
         {user ? <>
-            <Tooltip title='Schedule'>
-                <IconButton onClick={() => navigate('/')}>
-                    <Box
+            <IconButton onClick={handleClick}>
+                <Box
                     component={'img'}
                     sx={{
                         maxHeight: 50,
@@ -25,8 +43,30 @@ export default function Admin() {
                     }}
                     alt={'Schedule'}
                     src={resportsLogo}/>
-                </IconButton>
-            </Tooltip>
+            </IconButton>
+            
+            <Menu 
+                id='admin-menu'
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button'
+                }}>
+                    <MenuItem onClick={() => navigate('/')}>
+                        <ListItemIcon >
+                            <CalendarMonth fontSize='small'/>
+                        </ListItemIcon>
+                        <ListItemText>Schedule</ListItemText>
+                    </MenuItem>
+                    <MenuItem onClick={() => handleLogout()}>
+                        <ListItemIcon >
+                            <Logout  fontSize='small'/>
+                        </ListItemIcon>
+                        <ListItemText>Logout</ListItemText>
+                    </MenuItem>
+            </Menu>
+
             <Box
             display='flex'
             justifyContent='center'
