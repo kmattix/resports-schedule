86e4921bc6811221fe76fba58c8f4f32e25744b8
Kevin Mattix <kmattrider@gmail.com>
2023-09-06 18:03:18 -0400

    masked API key

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyCY_hMDBxq377NY4q33uneno9gBW_mbxXU",
-    authDomain: "resports-schedule.firebaseapp.com",
-    projectId: "resports-schedule",
-    storageBucket: "resports-schedule.appspot.com",
-    messagingSenderId: "947586957329",
-    appId: "1:947586957329:web:d658e8f7dac4b6ce8ef1b0",
-    measurementId: "G-S0YQRMQ8MG"
+    apiKey: "****************************************",
+    authDomain: "****************************************",
+    projectId: "****************************************",
+    storageBucket: "****************************************",
+    messagingSenderId: "****************************************",
+    appId: "****************************************",
+    measurementId: "****************************************"
   };
